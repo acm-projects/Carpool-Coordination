6f29438eaab0cdfc03b5ec6949db7d2bd7c5c710
farhan <Farrigalaxy321@gmail.com>
2022-03-01 20:33:21 -0600

    Firebase user authentication working

---
+++ android/app/src/main/kotlin/com/example/secondapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.secondapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/calendar_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+
+class CalendarScreen extends StatefulWidget {
+  const CalendarScreen({Key? key}) : super(key: key);
+
+  @override
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SfCalendar(),
+    );
+  }
+}
